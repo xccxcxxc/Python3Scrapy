9c637b3c41e90bd35288e7dc3f8ed0d7d43c37bc
xccxcxxc  <chenx212@gmail.com>
2022-09-15 22:08:56 +0800

    获取所有页面 url

--- 2-5.py
+++ 2-5.py
@@ -28,6 +28,10 @@ def scrape_index(page):
     return scrape_page(index_url)
 
 
+def scrape_detail(url):
+    return scrape_page(url)
+
+
 def parse_index(html):
     pattern = re.compile('<a.*?href="(.*?)".*class="name">')
     items = re.findall(pattern, html)
@@ -37,6 +41,40 @@ def parse_index(html):
         detail_url = urljoin(BASE_URL, item)
         logging.info('get detail url %s', detail_url)
         yield detail_url
+
+
+def parse_detail(html):
+    cover_pattern = re.compile(
+        'class="item.*?<img.*?src="(.*?)".*?class="cover>', re.S)
+    name_pattern = re.compile('<h2.*?>(.*?)</h2>')
+    categories_pattern = re.compile(
+        '<button.*?category.*?<span>(.*?)</span>.*?</button>', re.S
+    )
+    published_at_pattern = re.compile('(\d{4}-\d{2}-\d{2})\s?上映')
+    drama_pattern = re.compile('<div.*?drama.*?>.*?<p.*?>(.*?)</p>', re.S)
+    score_pattern = re.compile('<p.*?score.*?>(.*?)</p>', re.S)
+
+    cover = re.search(cover_pattern, html).group(1) \
+        .strip() if re.search(cover_pattern, html) else None
+    name = re.search(name_pattern, html).group(
+        1).strip() if re.search(name_pattern, html) else None
+    categories = re.findall(categories_pattern.html) if re.findall(
+        categories_pattern, html) else None
+    published_at = re.search(published_at_pattern, html).group(
+        1) if re.search(published_at_pattern, html) else None
+    drama = re.search(drama_pattern, html).group(1) \
+        .strip() if re.search(drama_pattern, html) else None
+    sore = float(re.search(score_pattern, html).group(1).strip()) \
+        if re.search(score_pattern, html) else None
+
+    return {
+        'cover': cover,
+        'name': name,
+        'categories': categories,
+        'published_at': published_at,
+        'drama': drama,
+        'score': score
+    }
 
 
 def main():
