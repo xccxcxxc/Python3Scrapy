1de44390bed351065856f3ce4dd4ab790a64ac89
xccxcxxc  <chenx212@gmail.com>
2022-09-24 22:41:26 +0800

    插入数据

---
+++ 4-4-3.py
@@ -0,0 +1,17 @@
+import pymysql
+
+id = '20120001'
+user = 'Bob'
+age = 20
+
+db = pymysql.connect(host='localhost', user='root',
+                     password='', port=3306, db='spiders')
+cursor = db.cursor()
+sql = 'INSERT INFO student(id, name, age) values(%s, %s, %s)'
+try:
+    cursor.execute(sql, (id, user, age))
+    db.commit()
+    print('Insert successfully')
+except:
+    db.rollback()
+db.close()
