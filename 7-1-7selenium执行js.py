94df32b2c3730326a0770abf9b5c1b025a47f2db
xccxcxxc  <chenx212@gmail.com>
2022-10-12 22:04:13 +0800

    selenium js

---
+++ 7-1-7selenium执行js.py
@@ -0,0 +1,7 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+browser = webdriver.Chrome()
+browser.get('https://www.zhihu.com/explore')
+browser.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+browser.execute_script('alert("To Bottom")')
