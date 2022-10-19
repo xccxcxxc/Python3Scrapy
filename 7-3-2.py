b2f2864e43fe8fedc1928c415917cbe1e7fe6408
xccxcxxc <chenx212@gmail.com>
2022-10-19 16:39:30 +0800

    文件敲完，运行失败

--- 7-3-2.py
+++ 7-3-2.py
@@ -1,18 +1,26 @@
 import asyncio
 from pyppeteer import launch
 
-width, heigh = 1366, 768
+width, height = 1366, 768
 
 
 async def main():
     browser = await launch()
     page = await browser.newPage()
-    await page.setViewport({'width': width, 'heigh': heigh})
+    await page.setViewport({'width': width, 'heigh': height})
     await page.goto('https://spa2.scrape.center/')
     await page.waitForSelector('.item .name')
-    doc = pq(await page.content())
-    names = [item.text() for item in doc('.item .name').items()]
-    print('Names:', names)
+    await asyncio.sleep(2)
+    await page.screenshot(path='example.png')
+    dimensions = await page.evaluate('''()=>{
+        return {
+            width:document.documentElement.clientWidth,
+            height:document.documentElement.clientHeight,
+            deviceScaleFactor: window.devicePixelRatio,
+        }
+    }
+    ''')
+    print(dimensions)
     await browser.close()
 
 
