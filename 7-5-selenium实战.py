4be5ba76f174250df877a5447c5a7eb164fb5694
xccxcxxc  <chenx212@gmail.com>
2022-10-23 22:20:56 +0800

    爬取到详情页 url

--- 7-5-selenium实战.py
+++ 7-5-selenium实战.py
@@ -21,3 +21,42 @@ options.add_experimental_option("excludeSwitches", ['enable-automation'])
 
 browser = webdriver.Chrome(chrome_options=options)
 wait = WebDriverWait(browser, TIMEOUT)
+
+
+def scrape_page(url, condition, locator):
+    logging.info('scraping %s', url)
+    try:
+        browser.get(url)
+        wait.until(condition(locator))
+    except:
+        logging.error('error occurred while scraping %s', url, exc_info=True)
+
+
+def scrape_index(page):
+    url = INDEX_URL.format(page=page)
+    scrape_page(url, condition=EC.visibility_of_element_located,
+                locator=(By.CSS_SELECTOR, '#index .item'))
+
+
+from urllib.parse import urljoin
+
+
+def parse_index():
+    elements = browser.find_elements(By.CSS_SELECTOR, '#index .item .name')
+    for element in elements:
+        href = element.get_attribute('href')
+        yield urljoin(INDEX_URL, href)
+
+
+def main():
+    try:
+        for page in range(1, TOTAL_PAGE + 1):
+            scrape_index(page)
+            detail_urls = parse_index()
+            logging.info('details urls %s', list(detail_urls))
+    finally:
+        browser.close()
+
+
+if __name__ == '__main__':
+    main()
