baa37e50da7ef0a08f6267ee28198fb42efadd60
xccxcxxc  <chenx212@gmail.com>
2022-10-25 22:10:31 +0800

    爬取电影细节

--- 7-5-selenium实战.py
+++ 7-5-selenium实战.py
@@ -55,12 +55,34 @@ def scrape_detail(url):
                 locator=(By.TAG_NAME, 'h2'))
 
 
+def parse_detail():
+    url = browser.current_url
+    name = browser.find_element(By.TAG_NAME, 'h2').text
+    categories = [element.text for element in browser.find_elements(By.CSS_SELECTOR, '.categories button span')]
+    cover = browser.find_element(By.CSS_SELECTOR, '.cover').get_attribute('src')
+    score = browser.find_element(By.CLASS_NAME, 'score').text
+    drama = browser.find_element(By.CSS_SELECTOR, '.drama p').text
+
+    return {
+        'url': url,
+        'name': name,
+        'categories': categories,
+        'cover': cover,
+        'score': score,
+        'drama': drama
+    }
+
+
 def main():
     try:
         for page in range(1, TOTAL_PAGE + 1):
             scrape_index(page)
             detail_urls = parse_index()
-            logging.info('details urls %s', list(detail_urls))
+            for detail_url in list(detail_urls):
+                logging.info('get detail url %s', detail_url)
+                scrape_detail(detail_url)
+                detail_data = parse_detail()
+                logging.info('detail data %s', detail_data)
     finally:
         browser.close()
 
