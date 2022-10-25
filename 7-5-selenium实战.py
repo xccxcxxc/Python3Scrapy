8e03afc8f314603851d4c6c011a78a5f6cb05aad
xccxcxxc  <chenx212@gmail.com>
2022-10-25 22:40:37 +0800

    临时

--- 7-5-selenium实战.py
+++ 7-5-selenium实战.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.chrome.options import Options
 
 from urllib.parse import urljoin
 
